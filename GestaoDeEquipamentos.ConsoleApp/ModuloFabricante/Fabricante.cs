52badcef286130856c014f7f469978e92b7e6c60
Kauã Biasebetti <kauamengel17@gmail.com>
2025-05-15 19:43:06 -0300

    Implementada validação de Fabricante

--- GestaoDeEquipamentos.ConsoleApp/ModuloFabricante/Fabricante.cs
+++ GestaoDeEquipamentos.ConsoleApp/ModuloFabricante/Fabricante.cs
@@ -1,4 +1,6 @@
-﻿namespace GestaoDeEquipamentos.ConsoleApp.ModuloFabricante;
+﻿using System.Net.Mail;
+
+namespace GestaoDeEquipamentos.ConsoleApp.ModuloFabricante;
 
 public class Fabricante
 {
@@ -12,5 +14,32 @@ public class Fabricante
         this.nome = nome;
         this.email = email;
         this.telefone = telefone;
+    }
+
+    public string Validar()
+    {
+        string erros = "";
+
+        if (string.IsNullOrEmpty(nome))
+        {
+            erros += "O nome é obrigatório!\n";
+        }
+
+        else if (nome.Length < 2)
+        {
+            erros += "O nome deve contar mais que um caractere!\n";
+        }
+
+        if (!MailAddress.TryCreate(email, out _))
+        {
+            erros += "O email deve conter um formato válido \"nome@provedor.com\"!\n";
+        }
+
+        if (string.IsNullOrWhiteSpace(telefone))
+        {
+            erros += "O telefone deve conter no mínimo 9 caracteres!\n";
+        }
+
+        return erros;
     }
 }
